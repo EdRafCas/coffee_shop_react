3e74d91464e0548237fb0b1c7136e051b7ddb226
EdRafCas <Eduardoo.rafael@gmail.com>
2021-05-22 21:19:37 -0300

    first commit, creating repository

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components'
+import BgImage from '../assets/bg.jpg'
+
+const Header = () => {
+      return ( 
+           <ContenedorHeader image={BgImage}>
+                 <Contenedor>
+                  Hola
+                 </Contenedor>
+           </ContenedorHeader>
+       );
+}
+
+const ContenedorHeader = styled.header`
+      background: url(${(props) => props.image || 'bg.jpg' });
+      background-size: cover;
+      width:100%;
+      background-attachment: fixed;
+      background-position: center 0; 
+      font-size: 30px;
+`;
+
+const Contenedor =styled.div`
+      position:relative;
+      height:700px;      
+`
+export default Header;
