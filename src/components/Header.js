73cfed843d2b60d8dab457b9c6104a3f28b54565
EdRafCas <Eduardoo.rafael@gmail.com>
2021-06-06 16:35:23 -0300

    separating scrollefect for nav menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,28 +13,66 @@ const Header = () => {
       }, []);
 
       let desplazamiento="";
-      if(offsetY>430){
-            desplazamiento = 430;
+      if(offsetY>520){
+            desplazamiento = 520;
       } else{
             desplazamiento= offsetY;
       }
 //-----------------------------------------------------------------------------------------------------//
-      let posicionesIniciales={divTexto:"-100", enlace:"-100"};
+      let posicionesIniciales={
+            op:"0",
+            divTexto:"350",
+            enlace1:"-100",
+            enlace2:"-150",
+            enlace3:"-200",
+            enlace4:"-250"
+      };
       
       const contador = () => setInterval(() => {
             contadorFuncional();  
       }, 10);
       const contadorFuncional = () => {
-            if(posicionesIniciales.enlace === 0){
+            if(posicionesIniciales.enlace1 === 0){
                   clearInterval(contador); 
             } else{
-                  posicionesIniciales.enlace++;
-                  setAltura(posicionesIniciales.enlace);
-            };     
+                  posicionesIniciales.enlace1++;
+                  setAltura(posicionesIniciales.enlace1);
+            };
+            if(posicionesIniciales.enlace2 === 0){
+                  clearInterval(contador); 
+            } else{
+                  posicionesIniciales.enlace2++;
+                  setAltura2(posicionesIniciales.enlace2);
+            };
+            if(posicionesIniciales.enlace3 === 0){
+                  clearInterval(contador); 
+            } else{
+                  posicionesIniciales.enlace3++;
+                  setAltura3(posicionesIniciales.enlace3);
+            };
+            if(posicionesIniciales.enlace4 === 0){
+                  clearInterval(contador); 
+            } else{
+                  posicionesIniciales.enlace4++;
+                  setAltura4(posicionesIniciales.enlace4);
+            };
+            if(posicionesIniciales.divTexto === 250){
+                  clearInterval(contador); 
+            } else{
+                  posicionesIniciales.divTexto--;
+                  posicionesIniciales.op++;
+                  setAltura5(posicionesIniciales.divTexto);
+                  setopacity(posicionesIniciales.op);
+            };
+
       };
       
       const [altura, setAltura]= useState(-100);
-      const [altura2, setAltura2]= useState(-100);
+      const [altura2, setAltura2]= useState(-150);
+      const [altura3, setAltura3]= useState(-200);
+      const [altura4, setAltura4]= useState(-250);
+      const [altura5, setAltura5]= useState(350);
+      const [opacity, setopacity]= useState(0);
 
       const handleAltura = () => setAltura(() =>{
             contador()}
@@ -45,20 +83,23 @@ const Header = () => {
            
       }, []);
 
+//------------------------------------------------------------------------------------------------------------------------------------------//
       
+
       return ( 
            <ContenedorHeader image={BgImage}>
                  <Contenedor>
                         <Menu>
-                              <EnlaceAcerca style={{top:`${altura}px`}}>Acerca de </EnlaceAcerca>
-                              <EnlaceMenu style={{top:`${altura}px`}}>Menu </EnlaceMenu>
-                              <EnlaceGaleria style={{top:`${altura}px`}}>Galeria</EnlaceGaleria>
-                              <EnlaceUbicacion style={{top:`${altura}px`}}>Ubicacion</EnlaceUbicacion>
+                              <Enlace style={{top:`${altura}px`}}>Acerca de </Enlace>
+                              <Enlace style={{top:`${altura2}px`}}>Menu </Enlace>
+                              <Enlace style={{top:`${altura3}px`}}>Galeria</Enlace>
+                              <Enlace style={{top:`${altura4}px`}}>Ubicacion</Enlace>
                         </Menu>
                         <Textos 
-                              style={{transform:`translateY(${desplazamiento*0.5}px)`, top:``}} 
+                              style={{transform:`translateY(${desplazamiento*0.5}px)`, top:`${altura5}px`, opacity:`${opacity/100}`}} 
+                              className={".sala-de-cafe"}
                         >
-                              <Nombre>Café</Nombre>
+                              <Nombre >Café</Nombre>
                               <Eduardo>Eduardo Castillo</Eduardo>
                               <Titulo>Lorem ipsum dolor sit amet consectetur adipisicing.</Titulo>
                         </Textos>
@@ -89,71 +130,25 @@ const Menu =styled.nav`
       width:100%;
       text-align:right;
 `
-const EnlaceAcerca = styled.a`
+const Enlace= styled.a`
       color: #fff;
       font-size: 20px;
       font-weight: 100;
       display: inline-block;
       margin: 40px 20px;
-      top:-100px;
       text-decoration: none;
       position: relative; 
       &:hover{
             text-decoration: underline;
       }
-
 `
-const EnlaceMenu = styled.a`
-      color: #fff;
-      font-size: 20px;
-      font-weight: 100;
-      display: inline-block;
-      margin: 40px 20px;
-      top:-150px;
-      text-decoration: none;
-      position: relative; 
-      &:hover{
-            text-decoration: underline;
-      }
-
-`
-const EnlaceGaleria = styled.a`
-      color: #fff;
-      font-size: 20px;
-      font-weight: 100;
-      display: inline-block;
-      margin: 40px 20px;
-      top:-200px;
-      text-decoration: none;
-      position: relative; 
-      &:hover{
-            text-decoration: underline;
-      }
-
-`
-const EnlaceUbicacion = styled.a`
-      color: #fff;
-      font-size: 20px;
-      font-weight: 100;
-      display: inline-block;
-      margin: 40px 20px;
-      top:-250px;
-      text-decoration: none;
-      position: relative; 
-      &:hover{
-            text-decoration: underline;
-      }
-
-`
-
-
-
 const Textos = styled.div`
       width: 100%;
       color: #fff;
       position:absolute;
-      top: 50%;     
-      opacity: 1; 
+      top: 50%;  
+      margin-top:48px;
+      opacity: 0; 
 `
 const Nombre =styled.h1`
       display:inline-block;
